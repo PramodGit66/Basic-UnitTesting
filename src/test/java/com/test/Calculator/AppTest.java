6c6589a6062be1a103b52133460a73eede85fe0c
PramodGit66 <ahirwarpramodkumar15@gmail.com>
2024-09-28 01:32:14 +0530

    commit for change

--- src/test/java/com/test/Calculator/AppTest.java
+++ src/test/java/com/test/Calculator/AppTest.java
@@ -2,12 +2,10 @@ package com.test.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-
+/*
 	private App calculator = null;
 
 	@BeforeAll
@@ -19,15 +17,17 @@ public class AppTest {
 	private void destroy() {
 		calculator = null;
 	}
-
+*/
 	@Test
 	public void testAdd() {
+		App calculator= new  App();
 		int actualResult = calculator.add(2, 3);
 		int expectedResult = 5;
 		assertEquals(expectedResult, actualResult);
 	}
     @Test
 	public void testMul() {
+    	App calculator = new App();
 		int actualResult = calculator.mul(4, 5);
 		int expectedResult = 20;
 		assertEquals(expectedResult, actualResult);
